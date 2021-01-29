1fef8eff5d6b867468f1552157f47ad43db565ff
premsharma0123 <prem@massmancybergeeks.com>
2021-01-29 14:42:11 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
     $(".hair-care").click(function(){
-        $(".drop_down").fadeToggle("slow");
+        $(".drop_down").slideToggle("slow");
     })
 })
+
+
+// ----slider-----section ---//
+
+    $('.slider1').owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    nav: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 1500,
+    singleItem: true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+     }
+});
