2e68916305b96e9a3d595c2de4085be990cd50c2
premsharma0123 <prem@massmancybergeeks.com>
2021-02-02 18:59:12 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,47 +1,62 @@
 $(document).ready(function(){
-    $(".hair-care").click(function(){
-        $(".drop_down").slideToggle("slow");
-    })
+  $(".hair-care").click(function(){
+      $(".drop3").slideToggle("");
+      // $(".drop2").slide("");
+  })
+  $(".slimming").click(function(){
+    $(".drop2").slideToggle("");
+    // $(".drop3").slideDown("");
+})
+$(".heading-btn").click(function(){
+    $(".weight_wrp").slideToggle("");
+})
+// $(".Banner_Wrapper,header").click(function(){
+//   $(".drop_down").slideUp("");
+// })
+// $(".Apt_heading h2").click(function(){
+//   $(".Form_wrp").slideToggle("");
+// })
+
 })
 
-  //  -----mouse---movement-----
+//  -----mouse---movement-----
 
-    //     $('.Treatment_Wrapper ').mousemove(function(e) {
-    //       var amountMovedX = (e.pageX * -0.53 / 6);
-    //       var amountMovedY = (e.pageY * -0.53 / 6);
-    //       $('.itemMove').css('left', amountMovedX + 'px');
-    //       $('.itemMove').css('top', amountMovedY + 'px');
-    //   });
+  //     $('.Treatment_Wrapper ').mousemove(function(e) {
+  //       var amountMovedX = (e.pageX * -0.53 / 6);
+  //       var amountMovedY = (e.pageY * -0.53 / 6);
+  //       $('.itemMove').css('left', amountMovedX + 'px');
+  //       $('.itemMove').css('top', amountMovedY + 'px');
+  //   });
 
-  //bar 
+//bar 
 
-  jQuery('.main-bar').click(function(e) {
-    jQuery(this).toggleClass('activefile');
-    e.preventDefault();
-    jQuery('.myHeader').slideToggle();
-  });
+jQuery('.main-bar').click(function(e) {
+  jQuery(this).toggleClass('activefile');
+  e.preventDefault();
+  jQuery('.myHeader').slideToggle();
+});
 
 // ----slider-----section ---//
 
-    $('.slider1').owlCarousel({
-    loop: true,
-    margin: 0,
-    autoplay: true,
-    nav: true,
-    autoplayTimeout: 4000,
-    smartSpeed: 1500,
-    singleItem: true,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut',
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-     }
+  $('.slider1').owlCarousel({
+  loop: true,
+  margin: 0,
+  autoplay: true,
+  nav: true,
+  autoplayTimeout: 4000,
+  smartSpeed: 1500,
+  singleItem: true,
+  animateIn: 'fadeIn',
+  animateOut: 'fadeOut',
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 1
+      },
+      1000: {
+          items: 1
+      }
+   }
 });
